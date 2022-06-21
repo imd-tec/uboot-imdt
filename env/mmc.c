6c740f7243c4ff8bca4b4da8e38a92e7bea0e6fa
paulthomson-imd <83579607+paulthomson-imd@users.noreply.github.com>
2022-06-21 14:57:27 +0100

    Remove hardcoded value for mmcroot
    The board_late_mmc_env_init() function no longer overrides the value for
    mmcroot. Modified the env_mmc_load() function to save the default
    environment if it hasn't already been written to the micro SD card.

--- env/mmc.c
+++ env/mmc.c
@@ -484,8 +484,14 @@ static int env_mmc_load(void)
 fini:
 	fini_mmc_for_env(mmc);
 err:
-	if (ret)
+	if (ret) {
 		env_set_default(errmsg, 0);
+
+		if (-ENOMSG == ret) {
+			printf("*** Saving default environment to MMC\n");
+			env_mmc_save();
+		}
+	}
 
 	return ret;
 }
